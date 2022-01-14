e1f161cdc2333a0d95de348bc0da29a94c2d7951
Amber Lin <amberlinyq@gmail.com>
2022-01-14 15:01:02 -0500

    Streamline with Nodemon

--- bash.js
+++ bash.js
@@ -1,7 +1,7 @@
 process.stdout.write('prompt >'); 
 process.stdin.on('data', (data) =>{
     const cmd = data.toString().trim();
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write(process.cwd());
     process.stdout.write('\nprompt >');
 })
 //hello
